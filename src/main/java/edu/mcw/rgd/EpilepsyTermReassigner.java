cae2332794493a002cf31b387b00989e50de0dbe
tutajm <mtutaj@mcw.edu>
2024-12-13 15:31:00 -0600

    epilepsy terms ressignment: added h=handling of mouse/human orthologs

--- src/main/java/edu/mcw/rgd/EpilepsyTermReassigner.java
+++ src/main/java/edu/mcw/rgd/EpilepsyTermReassigner.java
@@ -1,15 +1,18 @@
 package edu.mcw.rgd;
 
-import edu.mcw.rgd.dao.AbstractDAO;
+import edu.mcw.rgd.dao.impl.OrthologDAO;
+import edu.mcw.rgd.datamodel.Ortholog;
+import edu.mcw.rgd.datamodel.SpeciesType;
 import edu.mcw.rgd.process.Utils;
 
 import java.io.BufferedReader;
+import java.util.List;
 
 public class EpilepsyTermReassigner {
 
     public static void main(String[] args) throws Exception {
 
-        AbstractDAO dao = new AbstractDAO();
+        OrthologDAO dao = new OrthologDAO();
 
         //Gene symbol	Gene RGDID	old term	old term ID	new term	new term ID	reference RGDID
         //Gria2	61862	visual epilepsy	DOID:11832 	epilepsy	DOID:1826	737715
@@ -31,6 +34,20 @@ public class EpilepsyTermReassigner {
                     "WHERE annotated_object_rgd_id=? AND term_acc=? AND ref_rgd_id=?";
             int r = dao.update(sql, newTermAcc, geneRgdId, oldTermAcc, refRgdId);
             System.out.println("updated "+r+": newTermAcc="+newTermAcc+", geneRgdId="+geneRgdId+", oldTermAcc="+oldTermAcc+", refRgdId="+refRgdId);
+
+            // handle rat/mouse/human orthologs
+            List<Ortholog> orthologs = dao.getOrthologsForSourceRgdId(geneRgdId);
+            for( Ortholog o: orthologs ) {
+
+                if( o.getDestSpeciesTypeKey()==SpeciesType.RAT ||
+                    o.getDestSpeciesTypeKey()== SpeciesType.MOUSE ||
+                    o.getDestSpeciesTypeKey() == SpeciesType.HUMAN ) {
+
+                    r = dao.update(sql, newTermAcc, o.getDestRgdId(), oldTermAcc, refRgdId);
+                    System.out.println("  updated "+r+": ortholog:  newTermAcc="+newTermAcc+", geneRgdId="+o.getDestRgdId()+", oldTermAcc="+oldTermAcc+", refRgdId="+refRgdId);
+                }
+            }
+
         }
 
         in.close();
